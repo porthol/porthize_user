0626215c2df48cf3ba2825aa756b66bcc7254cde
Thomas Brunet <dev@thomas-brunet.fr>
2019-01-09 10:56:29 +0100

    default files created for user crud add method to getall

---
+++ src/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { UserService } from './user.service';
+import { Request, Response } from 'express';
+import * as HttpStatus from 'http-status';
+import { BaseResponse } from '../utils/BaseResponse';
+
+export class UserController {
+    protected UserService: UserService = UserService.get();
+
+    getAll(req: Request, res: Response): void {
+        this.UserService.getAll(req.body)
+        .then(users => {
+            res.status(HttpStatus.OK);
+            res.send(users);
+        })
+        .catch(err => {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+            res.send(new BaseResponse(HttpStatus.INTERNAL_SERVER_ERROR, 'Internal server error', err));
+        });
+    }
+}
