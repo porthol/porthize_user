8b63668e954748ee993f34c4b17cd69edf427f7b
Thomas Brunet <dev@thomas-brunet.fr>
2019-01-10 16:34:46 +0100

    correct bug on getAll user add hash password fnc update user model with required

---
+++ src/utils/hashPassword.ts
@@ -0,0 +1,13 @@
+import * as bcrypt from 'bcrypt';
+
+/**
+ * Function used to hash a password using bcrypt
+ *
+ * @export
+ * @param {string} password
+ * @param {number} [saltRounds=10]
+ * @returns {Promise<string>} Password hashed
+ */
+export function hashPassword(password: string, saltRounds: number = 10): Promise<string> {
+    return bcrypt.hash(password, saltRounds);
+}
