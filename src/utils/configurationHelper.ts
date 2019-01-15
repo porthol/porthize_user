b2164222f5cc57eae15f5a3b14d4f1ad2cde707a
Thomas Brunet <dev@thomas-brunet.fr>
2019-01-15 10:40:02 +0100

    update template to connect to multiple mongo update config to manage environment (dev, prod) add examples to test add helper to get connection database url remove use on environment variable add use of lodash to merge object (es merging is not good)

--- src/utils/configurationHelper.ts
+++ src/utils/configurationHelper.ts
@@ -1,22 +1,36 @@
 import { join, extname } from 'path';
 import { path } from 'app-root-path';
 import { CustomError, CustomErrorCode } from './CustomError';
+import * as _ from 'lodash';
 
 const CONFIGURATION: Map<string, object> = new Map();
 
-export function getConfiguration(configPath?: string): object {
+export function getConfiguration(configPath?: string, env?: string): object {
   try {
+    let envConfigPath;
     if (configPath) {
       configPath = join(path, configPath);
     } else {
       configPath = join(path, 'config', 'config.json');
+      switch (env) {
+        case 'development':
+            envConfigPath = join(path, 'config', 'config.dev.json');
+          break;
+        case 'production':
+            envConfigPath = join(path, 'config', 'config.prod.json');
+          break;
+      }
     }
 
     if (['.json'].indexOf(extname(configPath)) === -1) {
       throw new Error('Extension invalid.');
     } else {
       if (!CONFIGURATION.has(configPath)) {
-        const configObject = require(configPath);
+        let configObject = require(configPath);
+        const envConfigObject = require(envConfigPath);
+
+        configObject = _.merge(configObject,envConfigObject);
+
         CONFIGURATION.set(configPath, configObject);
       }
 
