6ed07a556da313974fbc9792fcf1f1e69197b2a0
Thomas Brunet <dev@thomas-brunet.fr>
2019-01-20 19:53:14 +0100

    add sample

---
+++ src/device/index.ts
@@ -0,0 +1,6 @@
+export * from './example.controller';
+export * from './example.routes';
+export * from './example.service';
+export * from './example.model';
+export * from './example.document';
+export * from './example.schema';
