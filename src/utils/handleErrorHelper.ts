1ba6603c5749954ab28d06ea8e315a0304fbd3cf
Thomas Brunet <dev@thomas-brunet.fr>
2019-01-21 13:42:18 +0100

    add handleError

---
+++ src/utils/handleErrorHelper.ts
@@ -0,0 +1,13 @@
+import { Response } from 'express';
+import * as httpStatus from 'http-status';
+import { CustomError, CustomErrorCodeToHttpStatus } from './CustomError';
+
+export function handleError(err, res: Response) {
+  if((typeof err).toString() === CustomError.toString()) {
+    res.status(CustomErrorCodeToHttpStatus(err.code))
+      .send(err);
+  }else{
+    res.status(httpStatus.INTERNAL_SERVER_ERROR)
+      .send(new CustomError(httpStatus.INTERNAL_SERVER_ERROR, 'Internal Server Error', err));
+  }
+}
