b00562ed599003c2f477f2b8434edfee883f9bed
Thomas Brunet <dev@thomas-brunet.fr>
2019-01-21 14:40:25 +0100

    use handle error in examlpe

--- src/example/example.controller.ts
+++ src/example/example.controller.ts
@@ -1,83 +1,76 @@
 import { ExampleService } from './example.service';
 import { Request, Response } from 'express';
 import * as httpStatus from 'http-status';
-import { CustomError } from '../utils/CustomError';
+import { CustomError, CustomErrorCode } from '../utils/CustomError';
+import { handleError } from '../utils/handleErrorHelper';
 
 export class ExampleController {
 
-    getAll(req: Request, res: Response): void {
-        ExampleService.get().getAll(req.query)
-            .then(examples => {
-                res.status(httpStatus.OK)
-                    .send(examples);
-            })
-            .catch(err => {
-                res.status(httpStatus.INTERNAL_SERVER_ERROR)
-                    .send(new CustomError(httpStatus.INTERNAL_SERVER_ERROR, 'Internal server error', err));
-            });
-    }
+  getAll(req: Request, res: Response): void {
+    ExampleService.get().getAll(req.query)
+      .then(examples => {
+        res.status(httpStatus.OK)
+          .send(examples);
+      })
+      .catch(err => {
+        handleError(err, res);
+      });
+  }
 
-    get(req: Request, res: Response): void {
-        ExampleService.get().get(req.params.id , req.query)
-            .then(example => {
-                if(!example){
-                    res.status(httpStatus.NOT_FOUND)
-                        .send(new CustomError(404, 'Example not found', null));
-                }else{
-                    res.status(httpStatus.OK)
-                        .send(example);
-                }
-            })
-            .catch(err => {
-                res.status(httpStatus.INTERNAL_SERVER_ERROR)
-                    .send(new CustomError(httpStatus.INTERNAL_SERVER_ERROR, 'Internal server error', err));
-            });
-    }
+  get(req: Request, res: Response): void {
+    ExampleService.get().get(req.params.id, req.query)
+      .then(example => {
+        if (!example) {
+          throw new CustomError(CustomErrorCode.ERRNOTFOUND, 'example not found');
+        } else {
+          res.status(httpStatus.OK)
+            .send(example);
+        }
+      })
+      .catch(err => {
+        handleError(err, res);
+      });
+  }
 
-    register(req: Request, res: Response): void {
-        ExampleService.get().create(req.body)
-            .then(example => {
-                res.status(httpStatus.CREATED)
-                    .send(example);
-            })
-            .catch(err => {
-                res.status(httpStatus.BAD_REQUEST)
-                    .send(new CustomError(httpStatus.INTERNAL_SERVER_ERROR, 'Bad request', err));
-            });
-    }
+  register(req: Request, res: Response): void {
+    ExampleService.get().create(req.body)
+      .then(example => {
+        res.status(httpStatus.CREATED)
+          .send(example);
+      })
+      .catch(err => {
+        handleError(err, res);
+      });
+  }
 
-    update(req: Request, res: Response): void {
-        ExampleService.get().update(req.params.id,req.body)
-            .then(result => {
-                if(result.nModified === 0){
-                    res.status(httpStatus.NOT_FOUND)
-                        .send(new CustomError(404, 'Example not found', null));
-                }else{
-                    // todo we should send back the example modified
-                    res.status(httpStatus.OK)
-                        .send(req.body);
-                }
-            })
-            .catch(err => {
-                res.status(httpStatus.BAD_REQUEST)
-                    .send(new CustomError(httpStatus.BAD_REQUEST, 'Bad request', err));
-            });
-    }
+  update(req: Request, res: Response): void {
+    ExampleService.get().update(req.params.id, req.body)
+      .then(result => {
+        if (result.nModified === 0) {
+          throw new CustomError(CustomErrorCode.ERRNOTFOUND, 'example not found');
+        } else {
+          // todo we should send back the example modified
+          res.status(httpStatus.OK)
+            .send(req.body);
+        }
+      })
+      .catch(err => {
+        handleError(err, res);
+      });
+  }
 
-    delete(req: Request, res: Response): void{
-        ExampleService.get().delete(req.params.id)
-            .then(result => {
-                if(result.n === 0){
-                    res.status(httpStatus.NOT_FOUND)
-                        .send(new CustomError(404, 'Example not found', null));
-                }else{
-                    res.status(httpStatus.NO_CONTENT)
-                        .send(result);
-                }
-            })
-            .catch(err => {
-                res.status(httpStatus.BAD_REQUEST)
-                    .send(new CustomError(httpStatus.BAD_REQUEST, 'Bad request', err));
-            });
-    }
+  delete(req: Request, res: Response): void {
+    ExampleService.get().delete(req.params.id)
+      .then(result => {
+        if (result.n === 0) {
+          throw new CustomError(CustomErrorCode.ERRNOTFOUND, 'example not found');
+        } else {
+          res.status(httpStatus.NO_CONTENT)
+            .send(result);
+        }
+      })
+      .catch(err => {
+        handleError(err, res);
+      });
+  }
 }
