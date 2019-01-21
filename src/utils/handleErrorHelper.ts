0ec6d4ad8a8442f967baafb12d8c13c42e73ba49
Thomas Brunet <dev@thomas-brunet.fr>
2019-01-21 13:46:38 +0100

    precise type for ts

--- src/utils/handleErrorHelper.ts
+++ src/utils/handleErrorHelper.ts
@@ -2,7 +2,7 @@ import { Response } from 'express';
 import * as httpStatus from 'http-status';
 import { CustomError, CustomErrorCodeToHttpStatus } from './CustomError';
 
-export function handleError(err, res: Response) {
+export function handleError(err : any, res: Response) {
   if((typeof err).toString() === CustomError.toString()) {
     res.status(CustomErrorCodeToHttpStatus(err.code))
       .send(err);
