564c2df29f200a87b8452cc09b9d3bfa72fb16e3
Thomas Brunet <dev@thomas-brunet.fr>
2019-02-28 14:34:18 +0100

    get workspace on init privilege and route

--- src/configure.ts
+++ src/configure.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import ExampleRoutes from './example/example.routes';
+import HealthRoutes from './utils/health.routes';
 import { serviceManager } from './utils/service.manager';
 import { ExampleService } from './example';
 
@@ -14,22 +15,11 @@ export function configureRouter(configuration?: object): express.Router[] {
     // Create a new router that will be exported and used by top-level app
     const router = express.Router();
 
-    /**
-     * Place here all middlewares that will be called BEFORE your API.
-     */
-    /**
-     * End of "before" middlewares declaration
-     */
-
     // Load all child routers that call your API.
     router.use('/api', ExampleRoutes);
 
-    /**
-     * Place here all middlewares that will be called AFTER your API.
-     */
-    /**
-     * End of "after" middlewares declaration
-     */
+    // health route
+    router.use('/api', HealthRoutes);
 
     // Create a private router
     const privateRouter = express.Router();
